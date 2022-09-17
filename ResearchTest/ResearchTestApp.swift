9583094308655ec0e3c2ef7d33bd8bf710dded0e
Bradley Cox <bradleycox1995@gmail.com>
2022-09-17 14:12:21 -0400

    Initial Commit

---
+++ ResearchTest/ResearchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResearchTestApp.swift
+//  ResearchTest
+//
+//  Created by Bradley Cox on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ResearchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
